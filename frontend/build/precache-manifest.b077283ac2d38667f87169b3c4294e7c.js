0b850c8ab60b76efbd200dd5c7e5d4e12d292687
unknown <manavs2110@gmail.com>
2022-12-08 03:07:00 +0530

    first commit

---
+++ frontend/build/precache-manifest.b077283ac2d38667f87169b3c4294e7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48eba0918d008f4182af",
+    "url": "/static/css/main.f60e384a.chunk.css"
+  },
+  {
+    "revision": "48eba0918d008f4182af",
+    "url": "/static/js/main.48eba091.chunk.js"
+  },
+  {
+    "revision": "189c21a7531ebe87652e",
+    "url": "/static/js/1.189c21a7.chunk.js"
+  },
+  {
+    "revision": "963b488c4127b6303081",
+    "url": "/static/css/2.6099978f.chunk.css"
+  },
+  {
+    "revision": "963b488c4127b6303081",
+    "url": "/static/js/2.963b488c.chunk.js"
+  },
+  {
+    "revision": "dcdeed5eed80026631be",
+    "url": "/static/js/runtime~main.dcdeed5e.js"
+  },
+  {
+    "revision": "0107fc8d1502ac07c4589f3a478b71c2",
+    "url": "/static/media/Bank_img.0107fc8d.png"
+  },
+  {
+    "revision": "b40255e429d1eb407807605fcecbe3b8",
+    "url": "/static/media/sparks.b40255e4.png"
+  },
+  {
+    "revision": "2d4b700d05eb499a455f48369d4d374c",
+    "url": "/index.html"
+  }
+];
