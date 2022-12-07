2e1b9958e3821d343fe1fe336fdae3fb25cfe50f
unknown <manavs2110@gmail.com>
2022-12-08 03:20:57 +0530

    third commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/users', userRoutes);
 
 //static production asset
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join("./frontend/build")));
+  app.use(express.static(path.join(__dirname,"frontend","build")));
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
   })
