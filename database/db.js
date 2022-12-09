cd2a8c2004bc4213d069fccf37b3332de4113173
unknown <manavs2110@gmail.com>
2022-12-09 23:22:59 +0530

    mongoDB bug solved 2

--- database/db.js
+++ database/db.js
@@ -10,6 +10,7 @@
 } catch (e) {
   console.log("could not connect");
 }*/
+console.log(process.env.MONGO_URI);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
