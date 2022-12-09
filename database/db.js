18848ff4c9bb1d409ced48b79f144ec7faa401b4
unknown <manavs2110@gmail.com>
2022-12-09 23:26:17 +0530

    mongoDB bug solved 2

--- database/db.js
+++ database/db.js
@@ -1,16 +1,6 @@
  const mongoose = require('mongoose');
 
-/*try {
-  // Connect to the MongoDB cluster
-  mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log(" Mongoose is connected"),
-  );
-} catch (e) {
-  console.log("could not connect");
-}*/
-console.log(process.env.MONGO_URI);
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
